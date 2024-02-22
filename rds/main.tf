e7d265097008c203281db7c57488a826c71da232
Jonathan Vinci <vincij002@gmail.com>
2024-02-22 18:23:43 -0500

    created rds module

--- rds/main.tf
+++ rds/main.tf
@@ -1,7 +1,7 @@
 # create database subnet group
 resource "aws_db_subnet_group" "database_subnet_group" {
   name        = "${var.project_name}-${var.environment}-database-subnets"
-  subnet_ids  = [var.aws_subnet.private_data_subnet_az1, var.aws_subnet.private_data_subnet_az2]
+  subnet_ids  = [var.private_data_subnet_az1, var.private_data_subnet_az2]
   description = "subnets for database instance"
 
   tags = {
