76a863074e95a2ec10d00b18520968924fc12d64
Jonathan Vinci <vincij002@gmail.com>
2024-02-21 22:33:21 -0500

    created rds module

---
+++ rds/variables.tf
@@ -0,0 +1,11 @@
+# rds variables
+variable "project_name" {}
+variable "environment" {}
+variable "aws_subnet.private_data_subnet_az1" {}
+variable "aws_subnet.private_data_subnet_az2" {}
+variable "database_snapshot_identifier" {}
+variable "database_instance_class" {}
+variable "availability_zone_1" {}
+variable "database_instance_identifier" {}
+variable "multi_az_deployment" {}
+variable "database_security_group_id" {}
